2652b1a4ce7f5f6820a9c6e21999825b4b1f7e86
lotus <daniel.younghwan.lee@gmail.com>
2022-07-17 13:33:17 -0400

    file set ups

---
+++ models/index.js
@@ -0,0 +1,11 @@
+// database named healthcare_db;
+
+// table called Provider
+// ID, Provider Name, Email, Password, Address, Specialization
+
+// table called Services
+// ID, Service, Price, Provider Name, Address, Specialization
+
+// table called Category
+// ID, Service_Category (Primary, Specialty, Emergency, Urgent, Long-term, Hospice, Mental), Service
+
