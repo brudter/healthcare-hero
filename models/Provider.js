ba3a87aa5bf5157367ff75b44edbc505863aefef
lotus <daniel.younghwan.lee@gmail.com>
2022-07-18 21:29:41 -0400

    still figuring out databases

--- models/Provider.js
+++ models/Provider.js
@@ -2,15 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// create our User model
 class Provider extends Model {
-  // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// create fields/columns for User model
 Provider.init(
   {
     id: {
@@ -76,7 +73,6 @@ Provider.init(
   },
   {
     hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newProviderData) {
         newProviderData.password = await bcrypt.hash(newProviderData.password, 10);
         return newProviderData;
