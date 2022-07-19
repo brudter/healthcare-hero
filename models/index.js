9f11627a9a4a8629547bfd1e8cc0c48ddbf6efc6
lotus <daniel.younghwan.lee@gmail.com>
2022-07-19 19:53:05 -0400

    healthcare hero set up

--- models/index.js
+++ models/index.js
@@ -1,21 +1,69 @@
-const Provider = require('./Provider');
-const Services = require('./Services');
-const Category = require('./Category');
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
-Provider.hasMany(Services, {
-    foreignKey: 'provider_id'
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
 });
 
-Services.belongsTo(Provider, {
-    foreignKey: 'provider_id'
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
-Category.hasMany(Services, {
-    foreignKey: 'category_id'
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
-Services.belongsTo(Category, {
-    foreignKey: 'category_id'
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
 });
 
-module.exports = { Provider, Services, Category };
+Vote.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
